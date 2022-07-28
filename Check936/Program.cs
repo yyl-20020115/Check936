43819a0b8d60c4c7235d936a37d80356f97c200f
YILIN <YILIN@DESKTOP-YILIN>
2022-07-29 06:26:21 +0800

    new

--- Check936/Program.cs
+++ Check936/Program.cs
@@ -1,37 +1,49 @@
-﻿if (args.Length == 0)
+﻿const char a1 = '\0';
+const char a2 = '\xff';
+const char c1 = '\ua1a1';
+const char c2 = '\u7e7e';
+
+if (args.Length == 0)
 {
-    Console.WriteLine("Usage: Check936 [file]");
-}
-else if (!File.Exists(args[0]))
-{
-    Console.WriteLine($"File {args[0]} does not exist!");
+    Console.WriteLine("Usage: Check936 [file] ... ");
 }
 else
 {
-    using var reader = new StreamReader(args[0]);
-    var text = "";
-    var line = 0;
-    var column = 0;
-    var a1 = '\0';
-    var a2 = '\xff';
-    var c1 = '\ua1a1';
-    var c2 = '\u7e7e';
-    while ((text = reader.ReadLine()) != null)
+    for (int i = 0; i < args.Length; i++)
     {
-        line++;
-        column = 0;
-        for (var i = 0; i < text.Length; i++)
+        var file = args[i];
+        if (!File.Exists(file))
         {
-            column++;
-            var c = text[i];
-            if (c >= a1 && c <= a2 || a2 >= c1 && a2 <= c2)
+            Console.WriteLine($"File {file} does not exist!");
+        }
+        else
+        {
+            Console.WriteLine($"Check {file} ...");
+            using (var reader = new StreamReader(file))
             {
-                //this is common char
-            }
-            else
-            {
-                //this char is out of GB2312(CP936)
-                Console.WriteLine($"Found char ({c}) which is not within CP936 at line {line}, column {column}");
+                var text = "";
+                var line = 0;
+                var column = 0;
+                while ((text = reader.ReadLine()) != null)
+                {
+                    line++;
+                    column = 0;
+                    for (var p = 0; p < text.Length; p++)
+                    {
+                        column++;
+                        var c = text[p];
+                        if (c >= a1 && c <= a2 || a2 >= c1 && a2 <= c2)
+                        {
+                            //this is common char
+                        }
+                        else
+                        {
+                            //this char is out of GB2312(CP936)
+                            Console.WriteLine($"Found char ('{c}':{(int)c:X4}) which is not within CP936 at line {line}, column {column}");
+
+                        }
+                    }
+                }
             }
         }
     }
